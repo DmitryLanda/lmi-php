46598a2ca9009bcf5e41e001300de0379dfc43b3
Dmitry Landa <dmitry.landa@opensoftdev.ru>
2015-01-21 00:55:20 +0400

    Add lost folder

---
+++ src/LmiSchool/Core/Authentication/AuthService.php
@@ -0,0 +1,91 @@
+<?php
+/*
+* For a full copyright notice, see the COPYRIGHT file.
+*/
+
+namespace LmiSchool\Core\Authentication;
+use Aura\Auth\Auth;
+use Aura\Auth\AuthFactory;
+use Aura\Auth\Verifier\PasswordVerifier;
+use Exception;
+use LmiSchool\Core\DatabaseConnection;
+
+/**
+ * @author Dmitry Landa <dmitry.landa@opensoftdev.ru>
+ */
+class AuthService
+{
+    private $auth;
+    private $loginService;
+    private $logoutService;
+    private $resumeService;
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @return Auth
+     */
+    public function login($username, $password)
+    {
+        try {
+            $this->loginService->login($this->auth, [
+                'username' => $username,
+                'password' => $password
+            ]);
+        } catch (Exception $e) {
+            //log it
+        }
+
+        return $this->auth;
+    }
+
+    public function logout(){
+        $this->logoutService->logout($this->auth);
+    }
+
+    /**
+     * @return Auth
+     */
+    public function resume()
+    {
+        $this->resumeService->resume($this->auth);
+
+        return $this->auth;
+    }
+
+    private function __construct()
+    {
+        $authFactory = new AuthFactory($_COOKIE);
+        $adapter = new DBALAdapter(
+            DatabaseConnection::getConnection(),
+            new PasswordVerifier(PASSWORD_BCRYPT),
+            [
+                'u.user',
+                'u.hash',
+                't
+                .id',
+                't.teacher_name',
+                't.teacher_foto'
+            ],
+            'users u JOIN teachers t ON t.teacher_id = u.teacher_id'
+        );
+        $this->loginService = $authFactory->newLoginService($adapter);
+        $this->logoutService = $authFactory->newLogoutService($adapter);
+        $this->resumeService = $authFactory->newResumeService($adapter);
+        $this->auth = $authFactory->newInstance();
+    }
+
+    private function __sleep() {}
+    private function __wakeup() {}
+    private function __clone() {}
+}
---
+++ src/LmiSchool/Core/Authentication/DBALAdapter.php
@@ -0,0 +1,72 @@
+<?php
+/*
+* For a full copyright notice, see the COPYRIGHT file.
+*/
+
+namespace LmiSchool\Core\Authentication;
+use Aura\Auth\Adapter\PdoAdapter;
+use Aura\Auth\Verifier\VerifierInterface;
+use Doctrine\DBAL\Connection;
+
+/**
+ * @author Dmitry Landa <dmitry.landa@opensoftdev.ru>
+ */
+class DBALAdapter extends PdoAdapter
+{
+    /**
+     * @var Connection
+     */
+    protected $connection;
+
+    /**
+     * @var array
+     */
+    protected $cols = array();
+
+    /**
+     * @var string
+     */
+    protected $from;
+
+    /**
+     * @var string|null
+     */
+    protected $where;
+
+    /**
+     * @var VerifierInterface
+     */
+    protected $verifier;
+
+    /**
+     * @param Connection $connection
+     * @param VerifierInterface $verifier
+     * @param array $cols
+     * @param string $from
+     * @param string|null $where
+     */
+    public function __construct(Connection $connection,
+                                VerifierInterface $verifier,
+                                array $cols,
+                                $from,
+                                $where = null)
+    {
+        $this->connection = $connection;
+        $this->verifier = $verifier;
+        $this->setCols($cols);
+        $this->from = $from;
+        $this->where = $where;
+    }
+
+    /**
+     * @param string $query
+     * @param array $bindings
+     * @return array
+     */
+    protected function fetchRows($query, array $bindings)
+    {
+        unset($bindings['password']);
+
+        return $this->connection->fetchAll($query, $bindings);
+    }
+}
